d173c8cb37514d42aeda7f683fa1eece3bd7c027
sergisantos658 <sergisantos658@gmail.com>
2022-04-01 20:55:16 +0200

    puzzle finished

---
+++ Assets/Scripts/puzzle/DragAndDrop_.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DragAndDrop_ : MonoBehaviour
+{
+    GameObject SelectedPiece;
+    public int numPieces;
+    int OIL = 1;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.transform.GetComponent<piecesScript>())
+            {
+                if (!hit.transform.GetComponent<piecesScript>().InRightPosition)
+                {
+                    SelectedPiece = hit.transform.gameObject;
+                    SelectedPiece.GetComponent<piecesScript>().Selected = true;
+                    SelectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL;
+                    OIL++;
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (SelectedPiece != null) 
+            {
+            SelectedPiece.GetComponent<piecesScript>().Selected = false;
+            SelectedPiece = null;
+            }
+        }
+
+
+        if (SelectedPiece != null)
+        {
+            Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            SelectedPiece.transform.position = new Vector3(MousePoint.x, MousePoint.y,0);
+        }
+    }
+}
