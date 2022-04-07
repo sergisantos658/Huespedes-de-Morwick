d63d43b6708fd379132df39f9414e6efb82f6eb8
MariaLopez-ifp <marialr134@gmail.com>
2022-04-07 18:10:00 +0200

    Doors Manager

---
+++ DoorsManager/Scripts/PlayerInteractive.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractive : MonoBehaviour
+{
+    public bool RedKey = false, BlueKey = false;
+    public Transform GoalPosition;
+}
---
+++ DoorsManager/Scripts/PlugScript.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlugScript : MonoBehaviour
+{
+    public bool OneTime = false;
+
+    public Transform HeroHandsPosition;
+
+    public Collider Socket;
+    public DoorScript DoorObject;
+
+    float distance;
+    float angleView;
+    Vector3 direction;
+
+    bool follow = false, isConnected = false, followFlag = false, youCan = true;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (youCan) Interaction();
+
+        if (isConnected)
+        {
+            gameObject.transform.position = Socket.transform.position;
+            gameObject.transform.rotation = Socket.transform.rotation;
+            DoorObject.isOpened = true;
+        }
+        else
+        {
+            DoorObject.isOpened = false;
+        }
+    }
+
+    void Interaction()
+    {
+        if (NearView() && Input.GetKeyDown(KeyCode.E) && !follow)
+        {
+            isConnected = false;
+            follow = true;
+            followFlag = false;
+        }
+
+        if (follow)
+        {
+            rb.drag = 10f;
+            rb.angularDrag = 10f;
+            if (followFlag)
+            {
+                distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+                if (distance > 3f || Input.GetKeyDown(KeyCode.E))
+                {
+                    follow = false;
+                }
+            }
+
+            followFlag = true;
+            rb.AddExplosionForce(-1000f, HeroHandsPosition.position, 10f);
+        }
+        else
+        {
+            rb.drag = 0f;
+            rb.angularDrag = .5f;
+        }
+    }
+
+    bool NearView()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        direction = transform.position - Camera.main.transform.position;
+        angleView = Vector3.Angle(Camera.main.transform.forward, direction);
+        if (distance < 3f && angleView <35f) return true;
+        else return false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == Socket)
+        {
+            isConnected = true;
+            follow = false;
+            DoorObject.rbDoor.AddRelativeTorque(new Vector3(0, 0, 20f));
+        }
+        if (OneTime) youCan = false;
+    }
+}
