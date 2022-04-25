3ca05c584d0d15450323af104987319ca0cb10c0
sergisantos <sergisantos658@gmail.com>
2022-04-25 14:17:30 +0200

    dialogues buu remaked

---
+++ Project/Assets/Scripts/InteractionSystem/Interactable2/ObjectsInteractables/RoteNPC.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoteNPC : Interactable
+{
+    // Start is called before the first frame update
+    public override void Interact()
+    {
+        Debug.Log("prueba");
+
+    }
+
+    public override void Observation()
+    {
+        
+    }
+}
