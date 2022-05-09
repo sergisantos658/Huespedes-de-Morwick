d31be9bf89e5e3ca5d5c23653368e86ff2d90f22
MariaLopez-ifp <marialr134@gmail.com>
2022-05-10 01:13:01 +0200

    Fold Inventory Done

---
+++ Project/Assets/Scripts/Menu/FoldInventory.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoldInventory : MonoBehaviour
+{
+	public Vector2 foldedPos;
+	Vector2 originalPos;
+	public float foldingTime;
+
+	[HideInInspector]
+	public bool isFolded = false;
+
+	void Start()
+	{
+		originalPos = transform.position;
+		isFolded = false;
+	}
+
+	void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Mouse2))
+        {
+			FoldInv();
+		}
+    }
+
+	public void FoldInv()
+	{
+		StartCoroutine(Fold());
+	}
+
+	IEnumerator Fold()
+	{
+		for (float time = 0f; time < foldingTime; time += Time.deltaTime)
+		{
+			if(!isFolded)
+            {
+				transform.position = Vector2.Lerp(originalPos, foldedPos, time / foldingTime);
+			}
+			
+			else
+			{
+				transform.position = Vector2.Lerp(foldedPos, originalPos, time / foldingTime);
+			}
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		isFolded = !isFolded;
+	}
+}
