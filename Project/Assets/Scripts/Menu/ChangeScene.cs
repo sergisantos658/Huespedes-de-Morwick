e329c2392278419a474df29213626ac92325c18b
Maria <marialr134@gmail.com>
2022-05-09 14:32:18 +0200

    Dialogs room 1 in progress

---
+++ Project/Assets/Scripts/Menu/ChangeScene.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public int sceneIndex;
+    // Start is called before the first frame update
+    public void SceneLoad()
+    {
+       MenuManager.pause = false;
+        Time.timeScale = 1; 
+        SceneManager.LoadScene(sceneIndex, LoadSceneMode.Single);
+        
+    }
+}
