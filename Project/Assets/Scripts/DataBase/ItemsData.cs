c5cfa5a1d9a55266d75d9dd7b6365c4215d72415
oscurio01 <izanr422@gmail.com>
2022-05-15 19:56:05 +0200

    Creating a system to save the objects that you collect

---
+++ Project/Assets/Scripts/DataBase/ItemsData.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ItemsData
+{
+
+    public string scriptedItemName;    
+
+    public ItemsData(Items items)
+    {
+        scriptedItemName = items.name;
+    }
+}
